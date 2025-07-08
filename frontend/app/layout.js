229b610f55de6f79da939926fc181be01b1f4765
nvdigitalsolutions <85486977+nvdigitalsolutions@users.noreply.github.com>
2025-07-08 20:08:03 +0530

    Load Prismic preview script globally

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,16 @@
+import Script from 'next/script'
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          defer
+          src="https://static.cdn.prismic.io/prismic.js?new=true&repo=prismic-nextjs-fastapi-dashboard"
+          strategy="afterInteractive"
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
